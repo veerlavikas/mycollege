f75223b9f7ebc12449cc8d00ebda24fae23a967c
veerlavikas <143020684+veerlavikas@users.noreply.github.com>
2025-09-17 11:25:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('showNotice').addEventListener('click', function () {
+    document.getElementById('notice').innerText = "📢 New semester classes start from 1st Oct 2025!";
+});
