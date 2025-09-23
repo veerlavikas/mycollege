5923538d0f3c2fc572e0fa044fed982d2570f029
veerlavikas <143020684+veerlavikas@users.noreply.github.com>
2025-09-23 11:28:52 +0530

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('showNotice').addEventListener('click', function () {
-    document.getElementById('notice').innerText = "📢 New semester classes start from 1st Oct 2025!";
-});
